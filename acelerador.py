c65ae7bf62a7731061d69e9987795e3a2170a017
Pedro Silva <pedroacsilva@gmail.com>
2022-07-29 13:19:40 +0100

    A enviar posicao do pedal. Necessario converter aceleracao (float) para bytes para enviar por CAN

--- acelerador.py
+++ acelerador.py
@@ -1,45 +1,35 @@
 #!/usr/bin/env python
 
 #Acelerador
-#Deverá receber uma mensagem CAN da ECU principal (slider na GUI). Por enquanto, recebi input do teclado (W)
-#A cada frame a que W é premido, incrementa a posição do acelerador
+#Deve ler a posição do pedal através da GUI ou potencialmente através do teclado.
 import keyboard
 import can
 import time
 from can.interface import Bus
 
-position = 0
-
-
 bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=500)
 
+position = 0
 
 
 while True:
     # Wait for the next event.
-    time.sleep(0.1)
-    event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN and event.name == 'w':
-        if position < 100:
-        	position += 5
-        	print("Posição do pedal: ", position)
-        	message = can.Message(data = position.to_bytes(8, 'big'))
-        	try:
-	        	bus.send(message)
-	        	print(f"Message sent on {bus.channel_info}.")
-        	except can.CanError:
-        			print("Message NOT sent")
+	time.sleep(0.1)
+	if(keyboard.is_pressed('w')):
+		position += 5
+	if(keyboard.is_pressed('s')):
+		position -= 5
+	if(keyboard.is_pressed('q')):
+		exit()
 
-    if event.event_type == keyboard.KEY_DOWN and event.name == 's':
-        if position > 0:
-        	position -= 5
-        	print("Posição do pedal: ", position)
-        	message = can.Message(data = position.to_bytes(8, 'big'))
-        	try:
-	        	bus.send(message)
-	        	print(f"Message sent on {bus.channel_info}.")
-        	except can.CanError:
-        			print("Message NOT sent")
+	message = can.Message(data = position.to_bytes(8, 'big'), arbitration_id = 0x01)
+	try:
+		bus.send(message)
+		print(f"Message sent on {bus.channel_info}. Position: ", position)
+	except can.CanError:
+		print("Message NOT sent")
 
-    if event.event_type == keyboard.KEY_DOWN and event.name == 'q':
-   		exit()
+
+
+	
+
---
+++ motor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#Motor
+#Deverá receber uma mensagem CAN do acelerador e produzir uma aceleração de acordo com a posição do pedal
+#Aceleração máxima é 5 m/s²(a 8000RPM)
+import can
+import time
+from can.bus import BusState
+
+
+bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=500)
+#bus.state = BusState.PASSIVE
+try:
+    while True:
+        msg = bus.recv(None)
+
+        if msg is not None and msg.arbitration_id == 0x01:
+#            print(msg)
+            posicao_pedal = int.from_bytes(msg, 'big')
+            rpm = posicao_pedal * 80
+            aceleracao = posicao_pedal / 20
+            print("RPM: ", rpm, "\nAceleração: ", aceleracao)
+        msg_out = can.Message(data = aceleracao.to_bytes(8, 'big'), arbitration_id = 0x03)
+        time.sleep(0.05)
+
+except KeyboardInterrupt:
+    pass
+
